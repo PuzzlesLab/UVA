b6e426d2313275b501f34d74a5bb2bb67f28a3aa
kingkingyyk <king8793yyk@hotmail.com>
2022-09-06 00:03:29 +0800

    Tue, Sep  6, 2022 12:03:29 AM: update all by king

--- King/00116 Unidirectional TSP.java
+++ King/00116 Unidirectional TSP.java
@@ -11,25 +11,22 @@ class Main {
 		if (!DpExists[m][n]) {
 			if (n==Nums[m].length-1) Dp[m][n]=Nums[m][n];
 			else {
-				int min=Integer.MAX_VALUE;
-
-				int best=Integer.MAX_VALUE;
-				int bestNextRow=-1;
+				int lowestCost=Integer.MAX_VALUE;
+				int lowestCostNextRow=-1;
 				for (int dm=-1;dm<2;dm++) {
 					int nm=m+dm;
 					if (nm<0) nm=Nums.length-1;
 					else if (nm>=Nums.length) nm=0;
 					
 					int curr=find(nm,n+1);
-					if (curr<best) {
-						best=curr;
-						bestNextRow=nm;
-					} else if (curr==best) bestNextRow=Math.min(bestNextRow, nm);
-
-					min=Math.min(min,curr+Nums[m][n]);
+					if (curr<lowestCost) {
+						lowestCost=curr;
+						lowestCostNextRow=nm;
+					} else if (curr==lowestCost) lowestCostNextRow=Math.min(lowestCostNextRow, nm);
 				}
-				Next[m][n]=bestNextRow;
-				Dp[m][n]=min;
+
+				Next[m][n]=lowestCostNextRow;
+				Dp[m][n]=lowestCost+Nums[m][n];
 			}
 			DpExists[m][n]=true;
 		}
@@ -51,28 +48,24 @@ class Main {
 			int ans=Integer.MAX_VALUE;
 			for (int m=0;m<M;m++) ans=Math.min(ans,find(m,0));
 
-			int minV=Integer.MAX_VALUE;
-			int minRow=Integer.MAX_VALUE;
+			int lowestCost=Integer.MAX_VALUE;
+			int lowestCostRow=Integer.MAX_VALUE;
 			for (int row=0;row<Nums.length;row++) {
-				if (Dp[row][0]<minV) {
-					minV=Dp[row][0];
-					minRow=row;
+				if (Dp[row][0]<lowestCost) {
+					lowestCost=Dp[row][0];
+					lowestCostRow=row;
 				}
 			}
 
-			int [] solution=new int [Nums[0].length];
-			int currRow=minRow;
+			StringBuilder sb=new StringBuilder();
+			int currRow=lowestCostRow;
 			for (int col=0;col<Nums[0].length;col++) {
-				solution[col]=currRow+1;
+				sb.append(currRow+1);
+				sb.append(' ');
 				currRow=Next[currRow][col];
 			}
-
-			StringBuilder sb=new StringBuilder();
-			for (int n=0;n<Nums[0].length;n++) {
-				sb.append(solution[n]);
-				sb.append(' ');
-			}
 			sb.setLength(sb.length()-1);
+
 			System.out.println(sb.toString());
 			System.out.println(ans);
 		}
