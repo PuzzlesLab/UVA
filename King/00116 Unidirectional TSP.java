6c287da0bb68fae65b069ec16873c37eceeb4b94
kingkingyyk <king8793yyk@hotmail.com>
2022-09-05 23:57:59 +0800

    Mon, Sep  5, 2022 11:57:59 PM: update all by king

--- King/00116 Unidirectional TSP.java
+++ King/00116 Unidirectional TSP.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
 
 	private static int [][] Nums;
+	private static boolean [][] DpExists;
 	private static int [][] Dp;
 	private static int [][] Next;
 
 	private static int find(int m, int n) {
-		if (Dp[m][n]==Integer.MAX_VALUE) {
+		if (!DpExists[m][n]) {
 			if (n==Nums[m].length-1) Dp[m][n]=Nums[m][n];
 			else {
 				int min=Integer.MAX_VALUE;
@@ -25,40 +25,15 @@ class Main {
 						best=curr;
 						bestNextRow=nm;
 					} else if (curr==best) bestNextRow=Math.min(bestNextRow, nm);
-					
-					Next[m][n]=bestNextRow;
+
 					min=Math.min(min,curr+Nums[m][n]);
 				}
+				Next[m][n]=bestNextRow;
 				Dp[m][n]=min;
 			}
+			DpExists[m][n]=true;
 		}
 		return Dp[m][n];
-	}
-
-	private static String trace() {
-		int minV=Integer.MAX_VALUE;
-		int minRow=Integer.MAX_VALUE;
-		for (int row=0;row<Nums.length;row++) {
-			if (Dp[row][0]<minV) {
-				minV=Dp[row][0];
-				minRow=row;
-			}
-		}
-
-		int [] solution=new int [Nums[0].length];
-		int currRow=minRow;
-		for (int col=0;col<Nums[0].length;col++) {
-			solution[col]=currRow+1;
-			currRow=Next[currRow][col];
-		}
-
-		StringBuilder sb=new StringBuilder();
-		for (int n=0;n<Nums[0].length;n++) {
-			sb.append(solution[n]);
-			sb.append(' ');
-		}
-		sb.setLength(sb.length()-1);
-		return sb.toString();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -69,15 +44,36 @@ class Main {
 			Nums=new int [M][N];
 			for (int m=0;m<M;m++) for (int n=0;n<N;n++) Nums[m][n]=sc.nextInt();
 			
+			DpExists=new boolean [M][N];
 			Dp=new int [M][N];
 			Next=new int [M][N];
-			for (int m=0;m<M;m++) Arrays.fill(Dp[m],Integer.MAX_VALUE);
-			for (int m=0;m<M;m++) Arrays.fill(Dp[m],Integer.MAX_VALUE);
 
 			int ans=Integer.MAX_VALUE;
 			for (int m=0;m<M;m++) ans=Math.min(ans,find(m,0));
 
-			System.out.println(trace());
+			int minV=Integer.MAX_VALUE;
+			int minRow=Integer.MAX_VALUE;
+			for (int row=0;row<Nums.length;row++) {
+				if (Dp[row][0]<minV) {
+					minV=Dp[row][0];
+					minRow=row;
+				}
+			}
+
+			int [] solution=new int [Nums[0].length];
+			int currRow=minRow;
+			for (int col=0;col<Nums[0].length;col++) {
+				solution[col]=currRow+1;
+				currRow=Next[currRow][col];
+			}
+
+			StringBuilder sb=new StringBuilder();
+			for (int n=0;n<Nums[0].length;n++) {
+				sb.append(solution[n]);
+				sb.append(' ');
+			}
+			sb.setLength(sb.length()-1);
+			System.out.println(sb.toString());
 			System.out.println(ans);
 		}
 	}
