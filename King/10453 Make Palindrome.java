967dc3045a8df09926ab16f9e6dd4fad7fb376fc
unknown <king8793yyk@hotmail.com>
2024-03-14 21:28:04 +0800

    14 Mar 2024 21:28:04: update all by king

---
+++ King/10453 Make Palindrome.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+
+	private static String Text;
+	private static int [][] Dp;
+	private static int NULL=100000;
+
+	private static int find(int i, int i2) {
+		if (i>i2) return NULL;
+		if (i==i2) return 0;
+		if (Dp[i][i2]==NULL) {
+			int ans=NULL+1;
+			if (Text.charAt(i)==Text.charAt(i2)) {
+				if (i+1==i2) ans=0;
+				else ans=find(i+1,i2-1);
+			} else ans=Math.min(1+find(i+1,i2),1+find(i,i2-1));
+			Dp[i][i2]=ans;
+		}
+		return Dp[i][i2];
+	}
+
+	private static void trace(int i, int i2, StringBuilder sb) {
+		if (i>i2) return;
+		if (Text.charAt(i)==Text.charAt(i2)) {
+			sb.append(Text.charAt(i));
+			trace(i+1,i2-1,sb);
+			if (i!=i2) sb.append(Text.charAt(i2));
+		} else if (find(i,i2)==1+find(i+1,i2)) {
+			sb.append(Text.charAt(i));
+			trace(i+1,i2,sb);
+			sb.append(Text.charAt(i));
+		} else if (find(i,i2)==1+find(i,i2-1)) {
+			sb.append(Text.charAt(i2));
+			trace(i,i2-1,sb);
+			sb.append(Text.charAt(i2));
+		}
+	}
+
+	public static void main (String [] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		while ((Text=br.readLine())!=null) {
+			Dp=new int [Text.length()][Text.length()];
+			for (int i=0;i<Dp.length;i++) Arrays.fill(Dp[i],NULL);
+			int ans=find(0,Text.length()-1);
+			StringBuilder sb=new StringBuilder();
+			sb.append(ans);
+			sb.append(' ');
+			trace(0,Text.length()-1,sb);
+			System.out.println(sb);
+		}
+	}
+}
